2f30dc8a2cfb819dd484894ccb61a41c64d4b067
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-28 14:44:04 -0800

    implemented the QuizBrain Class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -12,4 +13,6 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,3 +3,13 @@
 # TODO: Add asking Question
 # TODO: Add checking if the answer is valid
 # TODO: Add checking if we're the end of the quiz
+
+class QuizBrain:
+    def __init__(self, question_in_list):
+        self.question_number = 0
+        self.question_list = question_in_list
+
+    def next_question(self):
+        current = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"{self.question_number}:{current.text}(True/False): ")
