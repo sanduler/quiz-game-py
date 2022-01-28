477142fd28e73d2669aad096de334d533f364b4d
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-28 14:53:52 -0800

    implemented the next question

--- quiz_brain.py
+++ quiz_brain.py
@@ -9,6 +9,9 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_in_list
 
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
     def next_question(self):
         current = self.question_list[self.question_number]
         self.question_number += 1
