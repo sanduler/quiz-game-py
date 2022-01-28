6b57776c87de7d5f306fc80beb113afa61904a6b
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-28 14:27:48 -0800

    added a todo

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,1 +1,5 @@
 # Ruben Sanduleac
+
+# TODO: Add asking Question
+# TODO: Add checking if the answer is valid
+# TODO: Add checking if we're the end of the quiz
