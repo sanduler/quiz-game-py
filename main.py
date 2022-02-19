9043f4b66033dbe37b8d50978ec15fc5e2d7dfe5
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-19 15:24:34 -0800

    implemented the general UI

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizUI
 
 # create a blank list for the questions
 question_bank = []
@@ -21,10 +22,11 @@ for question in question_data:
 
 # initialize the QuizBrain class by passing the question bank
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizUI()
 
 # if the quiz still has questions the continue to the next question
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the Quiz")
 # print the final score
